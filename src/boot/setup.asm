654f47a149fa5556ad47b9acefb50b812b788407
willie <aaa87715@gmail.com>
2025-08-15 10:21:07 +0800

    Fix: align .data section with DS=CS by placing it after .text
    Previously, .data was placed before .text while ORG=0x500 and DS=CS.
    This caused label offsets in .data to be calculated relative to the data
    section, while DS pointed to the .text segment base, resulting in
    incorrect memory reads (e.g., lodsb reading garbage). Moved .data after
    .text so both share the same segment base.

--- src/boot/setup.asm
+++ src/boot/setup.asm
@@ -1,42 +1,47 @@
 [ORG 0x500]
 
-[SECTION .data]
-READ_BUFFER_START_ADDR equ 0x8002
-BUFFER_READ_COUNT_ADDR equ 0x8000
-BUFFER_MEMORY_USE_COUNT_ADDR equ 0x8001
-error_msg : db "memory initial fail!!!", 10, 13 , 0
-msg : db "hello word", 10, 13, 0
-read_count dw 0
-last_pos dw 0
-
 
 [SECTION .text]
 [BITS 16]
-call .print_hello_word
-xchg bx, bx ; 傳統「bochs magic breakpoint」
-call .init_memory_map_data
-xchg bx,bx ; 傳統「bochs magic breakpoint」
-call .read_memory_map
-call .after_memory_read
-jmp .done
+start:
+    xchg bx, bx ; 傳統「bochs magic breakpoint」
+    call .print_hello_word
+    call .init_memory_map_data
+    call .read_memory_map
+    call .after_memory_read
+    call .change_to_protect_mode
+    xchg bx,bx ; 傳統「bochs magic breakpoint」
+
+    jmp .done
+
+
+.change_to_protect_mode:
+
 
 .init_memory_map_data:
     mov ebx, 0
-    ;xor ebx, ebx ; init the ebx to 0
     mov ax, 0
     mov es, ax
     mov di, READ_BUFFER_START_ADDR
     ret
+
 .read_memory_map:
-    mov edx, 0x534D4150  ; SMAP
     mov eax, 0xE820  ;the memory map port
+    mov edx, 0x534D4150  ; SMAP
     mov ecx, 24     ;set bufffer size
     int 0x15
     jc .E820_fail
-    test ebx,ebx
+
+    cmp eax, 0x534D4150         ; must return 'SMAP'
+    jne .E820_fail
+
+
     mov [last_pos], di
     add di, 24
     inc word [read_count]
+
+    test ebx,ebx
+
     jnz .read_memory_map
     ret
 
@@ -53,17 +58,17 @@ jmp .done
     call .done
 
 
-.print_hello_word:
-    mov si ,msg
-    call .print
-    ret
-
-
 .print_memory_init_error:
     mov si ,error_msg
     call .print
     ret
 
+
+.print_hello_word:
+    mov si ,msg
+
+    call .print
+    ret
 
 .print:
     lodsb                 ; 從 si 指向的位址載入一個字元到 al，si++
@@ -77,4 +82,15 @@ jmp .done
     ret
 .done:
     jmp $
+
+
+[SECTION .data]
+READ_BUFFER_START_ADDR equ 0x8002
+BUFFER_READ_COUNT_ADDR equ 0x8000
+BUFFER_MEMORY_USE_COUNT_ADDR equ 0x8001
+error_msg : db "memory initial fail!!!", 10, 13 , 0
+msg : db "hello word", 10, 13, 0
+read_count dw 0
+last_pos dw 0
+
 times 1024 - ( $ - $$ ) db 0
